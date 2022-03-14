1005e58fa3f64c02e2d98438c4e6489300bab722
Tom <84847318+tomal02@users.noreply.github.com>
2022-03-14 11:32:48 +0000

    path fix

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
